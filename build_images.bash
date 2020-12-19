c5287ca016d713c7d0629fd1860e01eada71147b
Thomas Kent <tom@teeks99.com>
2020-12-19 06:58:06 -0600

    Easy to update version of boost

--- build_images.bash
+++ build_images.bash
@@ -1,9 +1,14 @@
+source boost_version.bash
 
 for compiler in clang-11 gcc-9
 do
   pushd $compiler
-  echo docker build -t teeks99/boost-python-test:$compiler .
-  docker build -t teeks99/boost-python-test:$compiler .
+  echo Building For: teeks99/boost-python-test:$compiler
+  docker build -t teeks99/boost-python-test:$compiler \
+    --build-arg release=$BOOST_RELEASE \
+    --build-arg release_file=$BOOST_RELEASE_FILE \
+    .
+  docker tag teeks99/boost-python-test:$compiler teeks99/boost-python-test:$compiler_$BOOST_RELEASE
   popd
 done
 
--- push_images.bash
+++ push_images.bash
@@ -1,6 +1,8 @@
+source boost_version.bash
 
 for compiler in clang-11 gcc-9
 do
   docker push teeks99/boost-python-test:$compiler
+  docker push teeks99/boost-python-test:$compiler_$BOOST_RELEASE
 done
 

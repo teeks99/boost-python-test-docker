65dfe3bc9c6ac25a02ee0da3db0b52977dd95a23
Thomas Kent <tom@teeks99.com>
2020-12-19 16:31:35 -0600

    Fixed bugs for 1.66.0

--- build_images.bash
+++ build_images.bash
@@ -1,14 +1,17 @@
+set -e
+set -o pipefail
+
 source boost_version.bash
 
 for compiler in clang-11 gcc-9
 do
   pushd $compiler
-  echo Building For: teeks99/boost-python-test:$compiler
-  docker build -t teeks99/boost-python-test:$compiler \
+  echo Building For: teeks99/boost-python-test:${compiler}
+  docker build --pull -t teeks99/boost-python-test:${compiler} \
     --build-arg release=$BOOST_RELEASE \
     --build-arg release_file=$BOOST_RELEASE_FILE \
     .
-  docker tag teeks99/boost-python-test:$compiler teeks99/boost-python-test:$compiler_$BOOST_RELEASE
+  docker tag teeks99/boost-python-test:${compiler} teeks99/boost-python-test:${compiler}_${BOOST_RELEASE}
   popd
 done
 
--- push_images.bash
+++ push_images.bash
@@ -2,7 +2,7 @@ source boost_version.bash
 
 for compiler in clang-11 gcc-9
 do
-  docker push teeks99/boost-python-test:$compiler
-  docker push teeks99/boost-python-test:$compiler_$BOOST_RELEASE
+  docker push teeks99/boost-python-test:${compiler}
+  docker push teeks99/boost-python-test:${compiler}_${BOOST_RELEASE}
 done
 

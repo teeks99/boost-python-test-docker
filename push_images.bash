880ab1e2605eb37234b7fa691fc610b2c8d39ac2
Thomas Kent <tom@teeks99.com>
2021-04-26 21:08:47 -0500

    Automatically tag git when pushing

--- push_images.bash
+++ push_images.bash
@@ -4,5 +4,8 @@ for compiler in clang-12 gcc-10
 do
   docker push teeks99/boost-python-test:${compiler}
   docker push teeks99/boost-python-test:${compiler}_${BOOST_RELEASE}
+
+  git tag ${compiler}_${BOOST_RELEASE}
+  git push origin ${compiler}_${BOOST_RELEASE}
 done
 
